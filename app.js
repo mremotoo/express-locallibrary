cda75045253a11b5b0d36647bc2b2c16baf1c829
mremoto <mremotoo@gmail.com>
2022-09-27 16:52:37 +0800

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -13,8 +13,13 @@ const helmet = require("helmet");
 var app = express();
 
 const mongoose = require("mongoose");
-const mongoDB =
+
+// Set up mongoose connection
+const dev_db_url =
   "mongodb+srv://marphilNodeTuts:tuts777@node-tuts.oww90tp.mongodb.net/local_library?retryWrites=true&w=majority";
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
